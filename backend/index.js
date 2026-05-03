bfc01da5a388b4a38770861093cd7ed74fa054fe
Rohitjargaal <rohitjargaal@gmail.com>
2026-05-03 11:40:57 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-website-lizz.onrender.com",
     credentials: true
 }));
 
